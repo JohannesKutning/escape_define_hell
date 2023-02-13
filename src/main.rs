2992c286e1442a2a999ad8795a046ea92ded66b9
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2023-02-13 18:06:57 +0100

    Adds a command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 use regex::Regex;
+use clap::{arg, command, Command, value_parser};
+
+fn cli() -> Command {
+    command!()
+        .arg( arg!( -i --input <FILE> "The source file to analyze." )
+                .required( true ) )
+        .arg( arg!( -l --line <INTEGER> "The line the ifdef report is generated for." )
+                .required( true )
+                .value_parser( value_parser!( usize ) ) )
+}
 
 fn main() {
-    let filename = "tests/test.c";
-    let line_number : usize = 12;
+    let matches = cli().get_matches();
+    let filename = matches.get_one::< String >( "input" ).expect( "required" );
+    let line_number : usize = * matches.get_one::< usize >( "line" ).expect( "required" );
     let stack = match read_and_parse_source_file( filename, line_number ) {
         Err( err ) => panic!( "error: {}", err ),
         Ok( stack ) => stack,
